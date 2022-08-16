5859917f472c4b0699c21473c49884f9467bc188
boxjan <i@boxjan.li>
2022-08-16 16:07:34 +0800

    fixed url parse

--- util.go
+++ util.go
@@ -4,8 +4,48 @@
 package gomaasapi
 
 import (
+	"fmt"
+	"regexp"
 	"strings"
 )
+
+var IgnoreTrailUrlSlashRegexPreCompile map[string]*regexp.Regexp
+var replaceReg *regexp.Regexp
+var DefaultIgnoreTrailSlashUrl = []string{
+	"license-key/{osystem}/{distro_series}",
+	"scripts/{name}",
+	"nodes/{system_id}/blockdevices/{device_id}/partition/{id}",
+}
+
+func init() {
+	var err error
+
+	// will replace `{.*}` => .*? for url regex string
+	replaceReg, _ = regexp.Compile("{.*?}")
+
+	err = ReloadRegex(DefaultIgnoreTrailSlashUrl)
+	if err != nil {
+		panic(fmt.Errorf("load ignore trail url regex failed with err: %v", err))
+	}
+}
+
+func ReloadRegex(regexUrl []string) error {
+
+	tmpRegexPreCompile := make(map[string]*regexp.Regexp)
+	for _, u := range regexUrl {
+		t := replaceReg.ReplaceAllString(u, ".*?")
+		tReg, err := regexp.Compile(t + "$")
+		// anyone compiled fail, will return error
+		if err != nil {
+			return fmt.Errorf("compile regix: `%s` failed with err: %+v", t, err)
+		}
+		tmpRegexPreCompile[u] = tReg
+	}
+
+	// all regexp string compile success
+	IgnoreTrailUrlSlashRegexPreCompile = tmpRegexPreCompile
+	return nil
+}
 
 // JoinURLs joins a base URL and a subpath together.
 // Regardless of whether baseURL ends in a trailing slash (or even multiple
@@ -20,6 +60,11 @@ func JoinURLs(baseURL, path string) string {
 // This is used to create the kind of normalized URLs that Django expects.
 // (to avoid Django's redirection when an URL does not ends with a slash.)
 func EnsureTrailingSlash(URL string) string {
+	for _, rgx := range IgnoreTrailUrlSlashRegexPreCompile {
+		if rgx.FindStringIndex(URL) != nil {
+			return URL
+		}
+	}
 	if strings.HasSuffix(URL, "/") {
 		return URL
 	}
